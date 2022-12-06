0e568ce7d08fb412e593286c7933e8f2e5472837
Kevin Li <yutengli@berkeley.edu>
2022-12-06 09:55:28 +0000

    added  sample_heuristic. finished first complete run of experiment.

--- cs285/explore/rho_explore_policy.py
+++ cs285/explore/rho_explore_policy.py
@@ -3,6 +3,7 @@ Implementation for algorithm 1 (perturbation-based exploration,
 without marginalizing over actions)
 """
 import numpy as np
+from scipy import stats
 import torch
 from gym import Env
 from copy import copy
@@ -19,6 +20,8 @@ class RhoExplorePolicy(object):
         self.look_ahead_steps = lmbda # FIXME: currently lambda always be 1. enable multi-step later.
         self.num_perturb_samples = rho_sample
         self.perturb_margin = rho
+        self.sample_heuristic = 'mode' # options: 'max', 'mode' (mode of top K percentile)
+        self.sample_threshold = .5
 
     def get_action(self, obs, env: Env, policy: BasePolicy):
         
@@ -32,14 +35,13 @@ class RhoExplorePolicy(object):
         
         perturbed_obs = self.perturb(observation) # (number of perturbed states, state dimension)
         scores_of_perturbed_obs = self.step_ahead(perturbed_obs, policy=policy) # (number of perturbed states, )
-        selected_obs = perturbed_obs[scores_of_perturbed_obs.argmax()] # (1, state dimension)
-        if len(selected_obs.shape) == 1:
-            selected_obs = selected_obs[None]
-        actions = policy.get_action(selected_obs)
-        # nearby_obs, nearby_qa_values = self.top_nearby_obs(collected_trajectories)
-        # nearby_qa_values: np.ndarray = self.critic.qa_values(nearby_obs) 
-        # greedy_actions = nearby_qa_values.argmax(axis=-1).squeeze() # (batch, # of nearby states)
-        # actions = ptu.from_numpy(greedy_actions).mode(dim=-1)
+        actions = self.sample_by_heuristic(
+            obs = perturbed_obs,
+            scores = scores_of_perturbed_obs,
+            heuristic = self.sample_heuristic,
+            policy = policy
+        ) # (1, state dimension)
+        
         return actions
 
     def perturb(self, obs):
@@ -89,4 +91,27 @@ class RhoExplorePolicy(object):
             self.env = copy(self.initial_state) # reset environment
             
         return np.array(qa_results)
-                
+
+    def sample_by_heuristic(self, obs, scores: np.ndarray, heuristic: str, policy: BasePolicy):
+        assert isinstance(scores, np.ndarray) and len(scores.shape) == 1
+        if heuristic == 'max':
+            selected_obs = obs[scores.argmax()]   
+        elif heuristic == 'mode':
+            assert hasattr(self, 'sample_threshold') 
+            pool_size = max(1, int(self.sample_threshold * len(scores)))
+            indices = scores.argsort()[-pool_size:] # ascending order
+            # print(f"indices: {indices}")
+            selected_obs = obs[indices] # (number of indices, state dimension)
+        else:
+            raise NotImplementedError("heuristic not identified") 
+        
+        if len(selected_obs.shape) == 1:
+            selected_obs = selected_obs[None]
+        actions = policy.get_action(selected_obs)
+
+        if isinstance(actions, np.ndarray):
+            assert len(actions.shape) == 1, "actions should be a rank-1 array"
+            actions = stats.mode(actions, keepdims=False).mode
+            return actions
+        else:
+            return actions
